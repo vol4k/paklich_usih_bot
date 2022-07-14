e196f703906ebda72ef2037f2854fc789ded66cf
Vadzim Valchkovich <vol4k.w@gmail.com>
2022-07-14 18:22:12 +0000

    upgrated flood control, added threading

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from telegram import Update, constants
+
+from time import sleep
+from threading import Thread
+
+from telegram import Message, Update, constants
 from telegram.ext import Updater, CallbackContext, CommandHandler
 
 TOKEN="PASTE_YOUR_TOKEN_HERE"
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
+
+lockedList: list[int] = []
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -14,7 +20,33 @@ def all(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Ну і каго мне тэгаць у асабоўцы?\nДадай мяне ў суполку!")
         return
 
-    call = " ".join(context.args)
+    delete_message(update, context)
+
+    if lock(update.effective_chat.id):
+        Thread(target=call_all, args=(update, context)).start()
+
+
+def help(update: Update, context: CallbackContext):
+    delete_message(update, context)
+
+    if not lock(update.effective_chat.id):
+        return
+
+    help_text = "Вітанкі! 👋\n\n\
+Я дапамагу табе паклікаць усіх, хто знаходзіцца разам з табой у суполцы.\n\n\
+Але перад тым як я гэта зраблю *дадай мяне ў гэтую суполку* каб я мог рэагаваць на паведамленні, а таксама *прызнач кіраўнікамі ўсіх, каго хочаш тэгаць*. \
+Не абавязкова выдаваць паўнамоцтвы кіраўнікам, галоўнае каб яны мелі статус.\n\
+А вось мне можаш даць *паўнамоцтвы на выдаленне паведамленняў*, калі хочаш, каб я выдаляў звароты да мяне.\n\
+Таксама звяртаю ўвагу на тое, што тэлеграм дазваляе мець толькі 50 кіраўнікоў \
+у адной суполцы, таму ў больш вялікіх суполках табе прыйдзецца добра падумаць каго па выніку мне давядзецца турбаваць\n\n\
+Калі прыйдзе час проста напішы /all у суполцы і я ўсіх паклічу. Апцыянальна можаш пасля /all дадаць свой заклік, напрыклад: `/all шатаць рэжым`"
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=help_text, parse_mode="Markdown")
+
+    unlock(update.effective_chat.id)
+
+
+def get_tag_messages(update: Update) -> list[str]:
     tagMessages = [""]
     tagsCounter = 0
 
@@ -25,34 +57,69 @@ def all(update: Update, context: CallbackContext):
         tagsCounter += 1
         if tagsCounter == 5:
             tagsCounter = 0
-            tagMessages[-1] += "\nБіп-буп, паведамленне яшчэ не знікла?\nПадаецца хтосьці схапіў бан за флуд, \nпачакай некалькі хвілін і спрабуй яшчэ раз 🤖"
             tagMessages.append("")
 
     if "" in tagMessages:
         tagMessages.remove("")
     
-    for tags in tagMessages:
-        try:
-            message = context.bot.send_message(chat_id=update.effective_chat.id, text=tags, parse_mode="Markdown", timeout=60)
-            message.edit_text(f"🔔 {update.effective_user.mention_markdown()} кліча усіх {call}", parse_mode="Markdown", timeout=60)
-        except:
-            print("Біп-буп, занадта шмат выклікаў, я зламаўся 🤖")
+    return tagMessages
 
+
+def call_all(update: Update, context: CallbackContext):
+    call = " ".join(context.args)
+    messages: list[Message] = []
+
+    tagMessages = get_tag_messages(update)
+
+    try:
+        for tags in tagMessages:
+            messages.append(message_call(update, context, tags, call))
+    except:
+        delete_messages(messages)
+        logging.error("Біп-буп, занадта шмат выклікаў, я зламаўся 🤖")
+
+    message = context.bot.send_message(chat_id=update.effective_chat.id, text=f"*🤖 Біп-буп, праца зроблена.*\n\n_Адыйшоў за гарбаткай ☕️\nПраз хвіліну вярнуся, не сумуйце без мяне 😊_", parse_mode="Markdown")
+
+    sleep(60)
+    message.delete()
+    unlock(update.effective_chat.id)
+
+
+def message_call(update: Update, context: CallbackContext, tags: str, call: str) -> Message:
+    sleep(2.5)
+
+    message = context.bot.send_message(chat_id=update.effective_chat.id, text=tags, parse_mode="Markdown", timeout=60)
+    message.edit_text(f"🔔 {update.effective_user.mention_markdown()} кліча усіх {call}", parse_mode="Markdown", timeout=60)
+
+    return message
+
+
+def delete_messages(messages: list[Message]):
+    for message in messages:
+        sleep(1)
+        message.delete()
+
+
+def delete_message(update: Update, context: CallbackContext):
     try:
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
     except:
         None
 
-def help(update: Update, context: CallbackContext):
-    help_text = "Вітанкі! 👋\n\n\
-Я дапамагу табе паклікаць усіх, хто знаходзіцца разам з табой у суполцы.\n\n\
-Але перад тым як я гэта зраблю *дадай мяне ў гэтую суполку* каб я мог рэагаваць на паведамленні, а таксама *прызнач кіраўнікамі ўсіх, каго хочаш тэгаць*. \
-Не абавязкова выдаваць паўнамоцтвы кіраўнікам, галоўнае каб яны мелі статус. Таксама звяртаю ўвагу на тое, што тэлеграм дазваляе мець толькі 50 кіраўнікоў \
-у адной суполцы, таму ў больш вялікіх суполках табе прыйдзецца добра падумаць каго па выніку я патурбую\n\n\
-Калі прыйдзе час проста напішы /all у суполцы і я ўсіх паклічу. Апцыянальна можаш пасля /all дадаць свой заклік, напрыклад: `/all шатаць рэжым`"
+def lock(chat_id: int) -> bool:
+    if chat_id not in lockedList:
+        lockedList.append(chat_id)
+        return True
+    else:
+        return False
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_text, parse_mode="Markdown")
-    context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
+def unlock(chat_id: int) -> bool:
+    if chat_id in lockedList:
+        lockedList.remove(chat_id)
+        return True
+    else:
+        return False
+
 
 all_handler = CommandHandler('all', all)
 start_handler = CommandHandler('start', help)
